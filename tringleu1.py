25ea76555baca57f08ffe96e6218184d14f2c91b
Brijeshm1 <113667620+Brijeshm1@users.noreply.github.com>
2022-09-16 13:25:19 +0530

    Add files via upload

---
+++ tringleu1.py
@@ -0,0 +1,14 @@
+
+x=int(input('enter no='))
+i=x
+while i>=1:
+    print(' '*(x-i)+'*'*i)
+    i=i-1
+'''
+x=int(input('enter no='))
+i=1
+while i<=x:
+    print(' '*(x-i)+'*'*i)
+    
+    i=i+1
+    '''
